7a02d71a6766a15bac2293e692600ad136bcc106
Santiago Saavedra <santiagosaavedra98@gmail.com>
2023-07-01 21:59:57 -0500

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        light: ['Breymont Light', 'system-ui'],
+        bold: ['Breymont Bold', 'system-ui']
       },
+      colors: {
+        'orange': '#F4A261'
+      }
     },
   },
   plugins: [],
